c561e1c072e49300154c9f9f149f415538f8f110
jeremycabrera1 <jeremycabrera1000@gmail.com>
2025-11-09 16:14:36 -0500

    adding problems i have already learn, will be adding the rest at a later time

--- linked_list/merge_two_sorted_lists.py
+++ linked_list/merge_two_sorted_lists.py
@@ -1,16 +1,34 @@
+# --------------------------------------------
 # Problem:
-# Given the head of a singly linked list, return the value of the middle node.
-# If there are two middle nodes, return the value of the second middle node.
+# You are given the heads of two sorted linked lists, list1 and list2.
+# Merge the two lists into one sorted linked list and return the head
+# of the merged list. The new list should be made by splicing together
+# the nodes of the first two lists.
+# --------------------------------------------
 
+# Definition for singly-linked list node.
 class Node:
     def __init__(self, val=0, next=None):
-        self.val = val        
-        self.next = next  
+        self.val = val
+        self.next = next
 
 
-def middle_of_linked_list(head:Node) -> int:
-    slow = fast = head
-    while fast and fast.next:
-        fast = fast.next.next 
-        slow = slow.next
-    return slow.val
+# --------------------------------------------
+# COMPLETE THIS FUNCTION (your code goes here)
+# --------------------------------------------
+def merge_two_sorted_lists(list1: Node, list2: Node) -> Node:
+    # Write your code here
+    dummy = Node(0)
+    tail = dummy
+
+    while list1 and list2:
+        if list1.val < list2.val:
+            tail.next = list1
+            list1 = list1.next
+        else:
+            tail.next = list2
+            list2 = list2.next
+        tail = tail.next
+
+    tail.next = list1 or list2
+    return dummy.next
