190879c7efd93298ef04cc3f9f81c92fee9194f7
jeremycabrera1 <jeremycabrera1000@gmail.com>
2025-11-09 18:13:32 -0500

    practicing

--- sliding_window/max_sum_subarray.py
+++ sliding_window/max_sum_subarray.py
@@ -10,13 +10,13 @@ k = 3
 
 
 def max_sum_subarray(arr:list, k:int) -> int:
-    window_sum = sum(arr[:k])
+    window_sum = sum(arr[:3])
     max_sum = window_sum
 
     for i in range(k, len(arr)):
         window_sum += arr[i] - arr[i - k]
         max_sum = max(max_sum, window_sum)
-    
+
     return max_sum
 
 print(max_sum_subarray(arr, k))
