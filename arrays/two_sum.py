be57cfa4e34e771349836cb87ff58e00932c70da
jeremycabrera1 <jeremycabrera1000@gmail.com>
2025-11-09 18:10:24 -0500

    practicing

--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -5,13 +5,16 @@
 
 
 def two_sum(numbers: list, target: int) -> list:
+    # Create a container for the indeces
     container = {}
-
+    # loop through the numbers using enumerate to get the indexes of the values
     for i, num in enumerate(numbers):
+        # Create a complement to check if the pair already exists in the container
         complement = target - num
-
+        # if the pair exists return the index of the complement and the current index we are looping through
         if complement in container:
             return [container[complement], i]
-        
+        # if the complement is not in the container add the current index and value to the container to run against in future loops in. key=num and value=i
         container[num] = i
+    #if nothing is found return an empty list
     return []
--- linked_list/merge_two_sorted_lists.py
+++ linked_list/merge_two_sorted_lists.py
@@ -29,6 +29,5 @@ def merge_two_sorted_lists(list1: Node, list2: Node) -> Node:
             tail.next = list2
             list2 = list2.next
         tail = tail.next
-
-    tail.next = list1 or list2
+    tail.next = list1 or list2    
     return dummy.next
