903a8db7dd4d6dc218ead5474390821622ace2ba
jeremycabrera1 <jeremycabrera1000@gmail.com>
2025-11-24 19:30:10 -0500

    practicing

---
+++ practice.py
@@ -0,0 +1,58 @@
+def maxArea(height: list[int]) -> int:
+    left, right = 0, len(height) - 1
+    max_area = 0
+
+    while left < right:
+        distance = right - left
+        current_height = min(height[left], height[right])
+        area = distance * current_height
+
+        max_area = max(area, max_area)
+
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+
+    return max_area
+
+
+def is_palindrome(s:str) -> bool:
+    left, right = 0, len(s) - 1
+
+    while left < right:
+        while left < right and not s[left].isalnum():
+            left += 1
+        while left < right and not s[right].isalnum():
+            right -= 1
+        if s[left].lower() != s[right].lower():
+            return False
+
+        left += 1
+        right -= 1
+
+    return True
+
+
+def reverseString(s: list[str]):
+    left, right = 0, len(s) - 1
+
+    while left < right:
+
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
+
+
+def twoSum(numbers: list[int], target: int) -> list[int]:
+    left, right = 0, len(numbers) - 1
+
+    while left < right:
+        current_sum = numbers[left] + numbers[right]
+
+        if current_sum < target:
+            left += 1
+        elif current_sum > target:
+            right -= 1
+        else:
+            return [left + 1, right + 1]
